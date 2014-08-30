ddee1a000767918c058d71d449583431d6599635
Tom Molesworth <tom@perlsite.co.uk>
2014-08-30 23:22:39 +0000

    Some documentation
    git-svn-id: http://entitymodel.com/svn/trunk/App-mirai@2565 c5bf8c3b-bf20-4ed3-b413-d5547a49f493

--- lib/App/mirai.pm
+++ lib/App/mirai.pm
@@ -1,5 +1,5 @@
 package App::mirai;
-# ABSTRACT: 
+# ABSTRACT: Monitor and debug Future objects
 use strict;
 use warnings;
 
@@ -13,9 +13,31 @@ App::mirai -
 
 =head1 SYNOPSIS
 
+ # start an IO::Async::Listener on the given port/socket file. Means the event loop needs to
+ # be running, but should be able to hook into an existing application without too much trouble.
+ # Some complications around Future nesting (Futures are created by the debugger itself) but
+ # that's easy enough to work around
+ perl -d:Mirai=localhost:1234 script.pl
+ perl -d:Mirai=/tmp/mirai.sock script.pl
+ 
+ # Run Tickit interface directly, presuming that the code itself is silent - everything is
+ # in-process, so no need for debugging to go via pipes
+ perl -Mirai script.pl
+ 
+ # Run the Tickit interface, and have it load the script as a separate process, directing
+ # STDOUT/STDERR to windows in the UI and communicating via pipepair
+ mirai script.pl
+
 =head1 DESCRIPTION
 
-未来
+Provides a basic debugging interface for tracing and interacting with L<Future>s. This should
+allow you to see the L<Future> instances currently in use in a piece of code, and what their
+current status is.
+
+The UI is currently L<Tickit>-based, there's a web interface in the works as well.
+
+Why the name? Mirai (未来) roughly translates to C< Future >, at least according to my limited
+Japanese vocabulary.
 
 =cut
 

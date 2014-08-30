ffafbb3750a0783263af77bc647a2648ddfc7600
Tom Molesworth <tom@perlsite.co.uk>
2014-08-30 23:22:38 +0000

    Include the Future patching code
    git-svn-id: http://entitymodel.com/svn/trunk/App-mirai@2562 c5bf8c3b-bf20-4ed3-b413-d5547a49f493

---
+++ lib/irai.pm
@@ -0,0 +1,21 @@
+package irai; 
+
+use strict;
+use warnings;
+
+use App::mirai::Future;
+
+1;
+
+__END__
+
+=head1 SEE ALSO
+
+=head1 AUTHOR
+
+Tom Molesworth <cpan@entitymodel.com>
+
+=head1 LICENSE
+
+Copyright Tom Molesworth 2014. Licensed under the same terms as Perl itself.
+
